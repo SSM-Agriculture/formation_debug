7f90dbc312eddeaa034fa01aa52aec2f3da21080
Damien Dotta <damien.dotta@live.fr>
2025-06-25 17:48:13 +0200

    chore: MAJ fichier exemple

--- R/exemples.R
+++ R/exemples.R
@@ -23,6 +23,28 @@ exemple1 <- function(indicateur) {
 
 exemple1("petale")
 
+
+# Breakpoints --------------------------------------------------------
+
+
+exemple1 <- function(indicateur) {
+  
+  if (indicateur == "Sepale") {
+    resultat <- iris |> 
+      select(Species,starts_with("Sepal")) |> 
+      head(3)
+  } else if (indicateur == "Petale") {
+    resultat <- iris |> 
+      select(Species,starts_with("Petal")) |> 
+      head(3)
+  }
+  return(resultat)
+  
+}
+
+exemple1("petale")
+
+
 # Browser -------------------------------------------------------------
 
 
@@ -46,8 +68,7 @@ exemple1 <- function(indicateur) {
 exemple1("petale")
 
 
-
-# Breakpoints --------------------------------------------------------
+# debug -------------------------------------------------------------
 
 
 exemple1 <- function(indicateur) {
@@ -65,5 +86,9 @@ exemple1 <- function(indicateur) {
   
 }
 
-exemple1("petale")
+debug(exemple1) # pour lancer le debuggeur sur la fonction
+exemple1("petale") # maintenant le debuggeur se lance qd on soumet la fonction
+undebug(exemple1) # pour arrêter le debuggeur sur la fonction
 
+debugonce(exemple1) # Pour lancer le debuggeur qu'une seule fois
+exemple1("petale") # le debuggeur ne va se lancer qu'une seule fois
