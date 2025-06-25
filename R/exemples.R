85d6a72ad1aeb5042cf52c65ce02e9e10d7a8e6f
Damien Dotta <damien.dotta@live.fr>
2025-06-25 16:11:22 +0200

    chore: éclatement fichier quarto

--- R/exemples.R
+++ R/exemples.R
@@ -1,5 +1,34 @@
 library(dplyr)
 
+
+# Print -------------------------------------------------------------
+
+
+exemple1 <- function(indicateur) {
+  
+  print(indicateur) # <- on ajout un print()
+  
+  if (indicateur == "Sepale") {
+    resultat <- iris |> 
+      select(Species,starts_with("Sepal")) |> 
+      head(3)
+  } else if (indicateur == "Petale") {
+    resultat <- iris |> 
+      select(Species,starts_with("Petal")) |> 
+      head(3)
+  }
+  return(resultat)
+  
+}
+
+exemple1("Petale")
+exemple1("petale")
+
+
+
+# Breakpoints --------------------------------------------------------
+
+
 exemple1 <- function(indicateur) {
   
   if (indicateur == "Sepale") {
@@ -15,6 +44,6 @@ exemple1 <- function(indicateur) {
   
 }
 
-# exemple1("Petale")
+exemple1("Petale")
 exemple1("petale")
 
