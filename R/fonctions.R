a18c08367f6f3e67bd29856cb22213895c36aeae
Damien Dotta <damien.dotta@live.fr>
2025-06-25 15:18:18 +0200

    feat: ajout fonctions.R

---
+++ R/fonctions.R
@@ -0,0 +1,14 @@
+exemple1 <- function(indicateur) {
+  
+  if (indicateur == "Sepale") {
+    resultat <- iris |> 
+      select(Species,starts_with("Sepal")) |> 
+      head(3)
+  } else if (indicateur == "Petale") {
+    resultat <- iris |> 
+      select(Species,starts_with("Petal")) |> 
+      head(3)
+  }
+  return(resultat)
+  
+}
